9ed369a404c58156eb19c74141f5026ae145e7c4
Saabithsp <saabithsp@gmail.com>
2025-07-05 21:25:51 +0530

    Added dynamic reflection for DiaryEntry class

--- src/controller/ReflectionUtility.java
+++ src/controller/ReflectionUtility.java
@@ -1,16 +1,19 @@
 package controller;
 
 import model.DiaryEntry;
-import java.lang.reflect.*;
 
-public class ReflectionUtil {
-    public static void inspectDiaryEntryClass() {
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ReflectionUtility {
+
+    public static void inspectDiaryEntry() {
         Class<?> clazz = DiaryEntry.class;
-        System.out.println("📘 Inspecting DiaryEntry class...");
+        System.out.println("\n🔍 Inspecting DiaryEntry class:");
 
         System.out.println("\nFields:");
         for (Field field : clazz.getDeclaredFields()) {
-            System.out.println("- " + field.getName() + ": " + field.getType().getSimpleName());
+            System.out.println("- " + field.getName() + " : " + field.getType().getSimpleName());
         }
 
         System.out.println("\nMethods:");
